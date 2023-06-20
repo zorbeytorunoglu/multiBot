3ed9f803cffda87f31146b0ffc4ce7097a7edb58
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-06-21 01:15:08 +0300

    Added EmbedMessageConfig

---
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/configuration/embedmessage/EmbedMessageConfig.kt
@@ -0,0 +1,16 @@
+package com.zorbeytorunoglu.multiBot.configuration.embedmessage
+
+data class EmbedMessageConfig(
+    val title: String?,
+    val titleUrl: String?,
+    val description: String?,
+    val author: String?,
+    val authorUrl: String?,
+    val authorIconUrl: String?,
+    val color: String?,
+    val footer: String?,
+    val footerUrl: String?,
+    val image: String?,
+    val thumbnail: String?,
+    val url: String?
+)
