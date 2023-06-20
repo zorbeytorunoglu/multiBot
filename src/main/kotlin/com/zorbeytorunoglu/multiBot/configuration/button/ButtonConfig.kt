27d44eae99f8dfec0b0aef6ed9f34886736bd9e0
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-06-21 01:13:57 +0300

    Added ButtonConfig

---
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/configuration/button/ButtonConfig.kt
@@ -0,0 +1,8 @@
+package com.zorbeytorunoglu.multiBot.configuration.button
+
+data class ButtonConfig(
+    val style: String,
+    val id: String,
+    val label: String?,
+    val emoji: String?
+)
