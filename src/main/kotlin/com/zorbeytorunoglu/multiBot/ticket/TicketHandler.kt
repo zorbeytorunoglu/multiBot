3b094c0997f117cc7202e4371d55dfadfbe47c82
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-06-25 04:45:18 +0300

    Added TicketHandler

---
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/ticket/TicketHandler.kt
@@ -0,0 +1,7 @@
+package com.zorbeytorunoglu.multiBot.ticket
+
+import com.zorbeytorunoglu.multiBot.ticket.configuration.TicketConfigurationHandler
+
+class TicketHandler(val ticketConfigurationHandler: TicketConfigurationHandler) {
+
+}
