07ac3eaaf35d317b438e4d5ab0f38f4adeb791aa
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-06-25 05:56:28 +0300

    Added ticket and messages handler.

--- src/main/kotlin/com/zorbeytorunoglu/multiBot/Bot.kt
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/Bot.kt
@@ -2,8 +2,10 @@ package com.zorbeytorunoglu.multiBot
 
 import com.zorbeytorunoglu.multiBot.commands.CommandsManager
 import com.zorbeytorunoglu.multiBot.commands.configuration.CommandsConfigurationHandler
+import com.zorbeytorunoglu.multiBot.messages.MessagesHandler
 import com.zorbeytorunoglu.multiBot.permissions.PermissionManager
 import com.zorbeytorunoglu.multiBot.settings.SettingsHandler
+import com.zorbeytorunoglu.multiBot.ticket.TicketHandler
 import net.dv8tion.jda.api.JDA
 
 class Bot {
@@ -13,5 +15,7 @@ class Bot {
     lateinit var permissionManager: PermissionManager
     lateinit var commandsConfigurationHandler: CommandsConfigurationHandler
     lateinit var commandsManager: CommandsManager
+    lateinit var ticketHandler: TicketHandler
+    lateinit var messagesHandler: MessagesHandler
 
 }
