32783ad160673e05965d2745e199fba5270ded7d
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-06-25 05:04:37 +0300

    Initialized updates

--- src/main/kotlin/com/zorbeytorunoglu/multiBot/Main.kt
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/Main.kt
@@ -3,8 +3,10 @@ package com.zorbeytorunoglu.multiBot
 import com.zorbeytorunoglu.multiBot.commands.CommandsManager
 import com.zorbeytorunoglu.multiBot.commands.Listener
 import com.zorbeytorunoglu.multiBot.commands.configuration.CommandsConfigurationHandler
+import com.zorbeytorunoglu.multiBot.messages.MessagesHandler
 import com.zorbeytorunoglu.multiBot.permissions.PermissionManager
 import com.zorbeytorunoglu.multiBot.settings.SettingsHandler
+import com.zorbeytorunoglu.multiBot.ticket.TicketHandler
 import com.zorbeytorunoglu.multiBot.ticket.configuration.TicketConfigurationHandler
 import net.dv8tion.jda.api.JDABuilder
 
@@ -20,11 +22,13 @@ fun main() {
 
     bot.jda = builder.build()
 
+    bot.messagesHandler = MessagesHandler()
     bot.commandsConfigurationHandler = commandsConfigurationHandler
     bot.permissionManager = permissionManager
     bot.commandsManager = CommandsManager(bot)
-    bot.jda.addEventListener(Listener(bot))
+    bot.settingsHandler = settingsHandler
+    bot.ticketHandler = TicketHandler(TicketConfigurationHandler())
 
-    TicketConfigurationHandler()
+    bot.jda.addEventListener(Listener(bot))
 
 }
