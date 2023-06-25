a2dcd4db1612f9af975bc0384720ee4384a32530
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-06-25 04:46:56 +0300

    Added Messages

---
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/messages/Messages.kt
@@ -0,0 +1,9 @@
+package com.zorbeytorunoglu.multiBot.messages
+
+data class Messages(
+
+    val ticketPanelSent: String = "Ticket panel is sent!",
+    val noPermission: String = "You have no permission!",
+    val ticketPanelIdOption: String = "The ID of the ticket panel (ticketPanels.json)"
+
+)
