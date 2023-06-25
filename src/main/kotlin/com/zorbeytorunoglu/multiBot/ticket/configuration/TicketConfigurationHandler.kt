717f0f18a45a4606316eaabda45c857fae7046dc
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-06-25 04:45:53 +0300

    Finished loading ticketPanels

--- src/main/kotlin/com/zorbeytorunoglu/multiBot/ticket/configuration/TicketConfigurationHandler.kt
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/ticket/configuration/TicketConfigurationHandler.kt
@@ -1,53 +1,80 @@
 package com.zorbeytorunoglu.multiBot.ticket.configuration
 
 import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
 import com.zorbeytorunoglu.multiBot.configuration.button.ButtonConfig
 import com.zorbeytorunoglu.multiBot.configuration.embedmessage.EmbedMessageConfig
-import com.zorbeytorunoglu.multiBot.ticket.TicketPanelConfig
+import com.zorbeytorunoglu.multiBot.utils.FileUtils
 import java.io.File
 
 class TicketConfigurationHandler {
 
     val ticketPanels = HashMap<String, TicketPanel>()
-    val ticketPanelConfigs = HashMap<String, TicketPanelConfig>()
 
     init {
         loadTicketPanels()
     }
 
-    fun loadTicketPanels() {
+    private fun loadTicketPanels() {
 
         val file = File("ticketPanels.json")
 
+        var ticketPanelConfigs = HashMap<String, TicketPanelConfig>()
+
         if (!file.exists()) {
 
-            val embedMessageConfig = EmbedMessageConfig("embedBaslik", null,
-                "aciklama",null,null,null,"ORANGE",null,
+            file.createNewFile()
+
+            val embedMessageConfig = EmbedMessageConfig("example", null,
+                "description","example",null,null,
+                "ORANGE","example",
                 null,null,null,null)
 
-            val buttonConfig = ButtonConfig("SECONDARY","buId","labelBu",null)
+            val buttonConfig = ButtonConfig("SECONDARY","example","example",null)
 
             val buttonsConfig: List<TicketButtonConfig> = listOf(
                 TicketButtonConfig(buttonConfig,"1203910239021")
             )
 
-            val ticketPanelConfig = TicketPanelConfig("baslik",embedMessageConfig,
+            val ticketPanelConfig = TicketPanelConfig("title-",embedMessageConfig,
                 buttonsConfig,"12031920",embedMessageConfig)
 
             ticketPanelConfigs["1"] = ticketPanelConfig
 
             val ticketPanel = TicketPanel(ticketPanelConfig)
 
+            ticketPanels["1"] = ticketPanel
+
             val gson = GsonBuilder().setPrettyPrinting().create()
 
             val json = gson.toJson(ticketPanelConfigs)
 
-            println("aha json")
-            println(json)
+            FileUtils.writeJsonToFile(json, file)
+
+        } else {
+
+            val json = FileUtils.readFile(file)
+
+            if (json == null) {
+                println("File ${file.name} could not be read due to an error.")
+                return
+            }
+
+            val type = object : TypeToken<HashMap<String, TicketPanelConfig>>() {}.type
+
+            val gson = GsonBuilder().setPrettyPrinting().create()
+
+            ticketPanelConfigs = gson.fromJson(json, type)
 
         }
 
-        //TODO: Return ticketPanels hashmap
+        if (ticketPanelConfigs.isNotEmpty()) {
+
+            ticketPanelConfigs.keys.forEach {
+                ticketPanels[it] = TicketPanel(ticketPanelConfigs[it]!!)
+            }
+
+        }
 
     }
 
