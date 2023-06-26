4053ad3bef5e5e850328b005dacf069dd018111b
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-06-26 04:03:17 +0300

    Using readyEvent for safe initializing

--- src/main/kotlin/com/zorbeytorunoglu/multiBot/Main.kt
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/Main.kt
@@ -1,34 +1,21 @@
 package com.zorbeytorunoglu.multiBot
 
-import com.zorbeytorunoglu.multiBot.commands.CommandsManager
-import com.zorbeytorunoglu.multiBot.commands.Listener
-import com.zorbeytorunoglu.multiBot.commands.configuration.CommandsConfigurationHandler
-import com.zorbeytorunoglu.multiBot.messages.MessagesHandler
-import com.zorbeytorunoglu.multiBot.permissions.PermissionManager
+import com.zorbeytorunoglu.multiBot.listeners.ReadyListener
 import com.zorbeytorunoglu.multiBot.settings.SettingsHandler
-import com.zorbeytorunoglu.multiBot.ticket.TicketHandler
-import com.zorbeytorunoglu.multiBot.ticket.configuration.TicketConfigurationHandler
 import net.dv8tion.jda.api.JDABuilder
 
 fun main() {
 
-    val permissionManager = PermissionManager()
     val settingsHandler = SettingsHandler()
-    val commandsConfigurationHandler = CommandsConfigurationHandler()
 
     val builder = JDABuilder.createDefault(settingsHandler.settings.token)
 
     val bot = Bot()
 
-    bot.jda = builder.build()
-
-    bot.messagesHandler = MessagesHandler()
-    bot.commandsConfigurationHandler = commandsConfigurationHandler
-    bot.permissionManager = permissionManager
-    bot.commandsManager = CommandsManager(bot)
     bot.settingsHandler = settingsHandler
-    bot.ticketHandler = TicketHandler(TicketConfigurationHandler())
 
-    bot.jda.addEventListener(Listener(bot))
+    builder.addEventListeners(ReadyListener(bot))
+
+    bot.jda = builder.build()
 
 }
