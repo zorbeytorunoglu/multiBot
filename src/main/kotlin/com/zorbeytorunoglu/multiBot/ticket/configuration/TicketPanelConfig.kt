589e95828cbfce662b53234f8e9639864312c234
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-06-26 04:08:01 +0300

    Improved the logic of TicketPanelConfig

--- src/main/kotlin/com/zorbeytorunoglu/multiBot/ticket/configuration/TicketPanelConfig.kt
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/ticket/configuration/TicketPanelConfig.kt
@@ -3,9 +3,6 @@ package com.zorbeytorunoglu.multiBot.ticket.configuration
 import com.zorbeytorunoglu.multiBot.configuration.embedmessage.EmbedMessageConfig
 
 data class TicketPanelConfig(
-    val titleFormat: String,
     val embedMessageConfig: EmbedMessageConfig,
-    val buttonsConfig: List<TicketButtonConfig>,
-    val ticketCategory: String,
-    val openingEmbed: EmbedMessageConfig
+    val buttonConfigs: List<TicketButtonConfig>
 )
