cb9e42bf4fb11e831ac4977b590745791e16b279
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-06-26 04:02:44 +0300

    Removed some default values

--- src/main/kotlin/com/zorbeytorunoglu/multiBot/configuration/embedmessage/EmbedMessageConfig.kt
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/configuration/embedmessage/EmbedMessageConfig.kt
@@ -1,14 +1,14 @@
 package com.zorbeytorunoglu.multiBot.configuration.embedmessage
 
 data class EmbedMessageConfig(
-    val title: String? = "example",
+    val title: String? = null,
     val titleUrl: String? = null,
-    val description: String? = "example",
-    val author: String? = "example",
+    val description: String? = null,
+    val author: String? = null,
     val authorUrl: String? = null,
     val authorIconUrl: String? = null,
     val color: String? = "ORANGE",
-    val footer: String? = "example",
+    val footer: String? = null,
     val footerUrl: String? = null,
     val image: String? = null,
     val thumbnail: String? = null,
