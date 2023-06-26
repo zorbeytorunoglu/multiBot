89913ae960aa8c5813977c9b91577395ee257682
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-06-26 04:04:54 +0300

    Added few variables for planned features.

--- src/main/kotlin/com/zorbeytorunoglu/multiBot/ticket/configuration/TicketButtonConfig.kt
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/ticket/configuration/TicketButtonConfig.kt
@@ -1,8 +1,13 @@
 package com.zorbeytorunoglu.multiBot.ticket.configuration
 
 import com.zorbeytorunoglu.multiBot.configuration.button.ButtonConfig
+import com.zorbeytorunoglu.multiBot.configuration.embedmessage.EmbedMessageConfig
 
 data class TicketButtonConfig(
     val buttonConfig: ButtonConfig,
-    val targetCategoryId: String
+    val targetCategoryId: String,
+    val channelTitleFormat: String,
+    val openingEmbed: EmbedMessageConfig? = null,
+    val rolesToBeAdded: String? = null
+
 )
