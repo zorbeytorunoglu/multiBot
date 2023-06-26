d51769ee85a0c493ccd51fb796f86de1c067df73
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-06-26 04:04:19 +0300

    Variable in constructor is not private for better utility.

--- src/main/kotlin/com/zorbeytorunoglu/multiBot/ticket/configuration/TicketButton.kt
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/ticket/configuration/TicketButton.kt
@@ -2,7 +2,7 @@ package com.zorbeytorunoglu.multiBot.ticket.configuration
 
 import com.zorbeytorunoglu.multiBot.configuration.button.Button
 
-class TicketButton(private val ticketButtonConfig: TicketButtonConfig) {
+class TicketButton(val ticketButtonConfig: TicketButtonConfig) {
 
     val button = Button(ticketButtonConfig.buttonConfig)
     val targetCategoryId = ticketButtonConfig.targetCategoryId
