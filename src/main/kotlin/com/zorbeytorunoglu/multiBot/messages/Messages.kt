40c3b78002c27ee6abfa56575bad428630fce7ec
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-06-26 04:03:31 +0300

    Added few messages

--- src/main/kotlin/com/zorbeytorunoglu/multiBot/messages/Messages.kt
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/messages/Messages.kt
@@ -4,6 +4,8 @@ data class Messages(
 
     val ticketPanelSent: String = "Ticket panel is sent!",
     val noPermission: String = "You have no permission!",
-    val ticketPanelIdOption: String = "The ID of the ticket panel (ticketPanels.json)"
+    val ticketPanelIdOption: String = "The ID of the ticket panel (ticketPanels.json)",
+    val targetCategoryNotFound: String = "Target category could not be found.",
+    val ticketReady: String = "Your ticket is ready! %ticket%"
 
 )
