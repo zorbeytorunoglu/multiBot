13974c0d26ea66071d89d7fafeed667817157b6e
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-06-28 09:23:17 +0300

    Added a new message

--- src/main/kotlin/com/zorbeytorunoglu/multiBot/messages/Messages.kt
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/messages/Messages.kt
@@ -6,6 +6,7 @@ data class Messages(
     val noPermission: String = "You have no permission!",
     val ticketPanelIdOption: String = "The ID of the ticket panel (ticketPanels.json)",
     val targetCategoryNotFound: String = "Target category could not be found.",
-    val ticketReady: String = "Your ticket is ready! %ticket%"
+    val ticketReady: String = "Your ticket is ready! %ticket%",
+    val exceedTicketLimit: String = "You can not exceed your ticket limit!"
 
 )
