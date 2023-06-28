0fca17212a900d9a5564f1be562329ec3ce4c0a3
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-06-28 09:26:06 +0300

    Improved ticket system in many ways.

--- src/main/kotlin/com/zorbeytorunoglu/multiBot/ticket/TicketManager.kt
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/ticket/TicketManager.kt
@@ -3,11 +3,15 @@ package com.zorbeytorunoglu.multiBot.ticket
 import com.zorbeytorunoglu.multiBot.ticket.configuration.TicketButton
 import com.zorbeytorunoglu.multiBot.ticket.configuration.TicketConfigurationHandler
 import com.zorbeytorunoglu.multiBot.ticket.configuration.TicketPanel
+import net.dv8tion.jda.api.entities.Member
+import net.dv8tion.jda.api.entities.channel.concrete.TextChannel
 import net.dv8tion.jda.api.interactions.components.buttons.Button
 
 class TicketManager(val ticketConfigurationHandler: TicketConfigurationHandler) {
 
     fun isTicketPanelButton(button: Button): Boolean {
+
+        if (ticketConfigurationHandler.ticketPanels.isEmpty()) return false
 
         ticketConfigurationHandler.ticketPanels.values.forEach { ticketPanel ->
             ticketPanel.buttons.forEach {
@@ -21,6 +25,8 @@ class TicketManager(val ticketConfigurationHandler: TicketConfigurationHandler) 
 
     fun getTicketPanel(button: Button): TicketPanel? {
 
+        if (ticketConfigurationHandler.ticketPanels.isEmpty()) return null
+
         ticketConfigurationHandler.ticketPanels.values.forEach { ticketPanel ->
             ticketPanel.buttons.forEach {
                 if (it.button.id == button.id) return ticketPanel
@@ -33,6 +39,8 @@ class TicketManager(val ticketConfigurationHandler: TicketConfigurationHandler) 
 
     fun getTicketButton(button: Button): TicketButton? {
 
+        if (ticketConfigurationHandler.ticketPanels.isEmpty()) return null
+
         ticketConfigurationHandler.ticketPanels.values.forEach { ticketPanel ->
             ticketPanel.buttons.forEach {
                 if (it.button.id == button.id) return it
@@ -41,6 +49,54 @@ class TicketManager(val ticketConfigurationHandler: TicketConfigurationHandler) 
 
         return null
 
+    }
+
+    fun getMemberTickets(member: Member): Collection<TextChannel> {
+
+        val tickets = mutableListOf<TextChannel>()
+
+        if (member.guild.textChannels.isEmpty()) return tickets
+
+        member.guild.textChannels.forEach {
+            if (isMembersTicket(member, it))
+                tickets.add(it)
+        }
+
+        return tickets
+
+    }
+
+    fun isMembersTicket(member: Member, textChannel: TextChannel): Boolean {
+
+        if (textChannel.topic == null) return false
+
+        val args = textChannel.topic!!.split("-")
+
+        if (args.size != 3) return false
+
+        return args[1] == member.id
+
+    }
+
+    fun getTicketLimit(buttonId: String): Int {
+
+        if (ticketConfigurationHandler.ticketPanels.isEmpty()) return 0
+
+        for (panel in ticketConfigurationHandler.ticketPanels.values) {
+            if (panel.buttons.isEmpty()) continue
+            for (button in panel.buttons) {
+                if (button.button.id == buttonId) {
+                    return button.ticketButtonConfig.limit
+                }
+            }
+        }
+
+        return 0
+
+    }
+
+    fun exceededTicketLimit(member: Member, buttonId: String): Boolean {
+        return getTicketLimit(buttonId) <= getMemberTickets(member).size
     }
 
 }
--- src/main/kotlin/com/zorbeytorunoglu/multiBot/ticket/listeners/TicketButtonListener.kt
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/ticket/listeners/TicketButtonListener.kt
@@ -2,8 +2,12 @@ package com.zorbeytorunoglu.multiBot.ticket.listeners
 
 import com.zorbeytorunoglu.multiBot.Bot
 import com.zorbeytorunoglu.multiBot.configuration.embedmessage.EmbedMessage
+import com.zorbeytorunoglu.multiBot.configuration.embedmessage.EmbedMessageBuilder
+import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import java.lang.StringBuilder
+import java.util.concurrent.TimeUnit
 
 class TicketButtonListener(private val bot: Bot): ListenerAdapter() {
 
@@ -11,8 +15,15 @@ class TicketButtonListener(private val bot: Bot): ListenerAdapter() {
 
         if (!event.isFromGuild) return
 
+        if (event.button.id == null) return
+
         val ticketButton =
             bot.ticketHandler.getTicketButton(event.button) ?: return
+
+        if (bot.ticketHandler.exceededTicketLimit(event.member!!, event.button.id!!)) {
+            event.reply(bot.messagesHandler.messages.exceedTicketLimit).setEphemeral(true).queue()
+            return
+        }
 
         if (event.guild!!.getCategoryById(ticketButton.targetCategoryId) == null) {
             event.reply(bot.messagesHandler.messages.targetCategoryNotFound).setEphemeral(true)
@@ -20,17 +31,75 @@ class TicketButtonListener(private val bot: Bot): ListenerAdapter() {
         }
 
         val title = ticketButton.ticketButtonConfig.channelTitleFormat
-            .replace("%ticket_id%", "1")
             .replace("%member_id%", event.member!!.id)
 
         val category = event.guild!!.getCategoryById(ticketButton.targetCategoryId)!!
 
         category.createTextChannel(title).queue {
-            //TODO: Register ticket
+
+            //TODO: Adding roles and the member to the ticket may be improved, need to learn more about JDA
+
+            //TODO: Can make permissions configurable
+
+            it.manager.putPermissionOverride(it.guild.publicRole, mutableListOf(), mutableListOf(Permission.VIEW_CHANNEL)).queue {void ->
+
+                it.manager.putMemberPermissionOverride(event.member!!.idLong,
+                    mutableListOf(Permission.VIEW_CHANNEL, Permission.MESSAGE_SEND,
+                        Permission.MESSAGE_HISTORY, Permission.MESSAGE_ATTACH_FILES,
+                        Permission.MESSAGE_ADD_REACTION), mutableListOf()
+                ).queue { void2 ->
+                    if (ticketButton.ticketButtonConfig.rolesToBeAdded != null) {
+                        it.manager.setTopic("ticket-"+event.member!!.id+"-"+ticketButton.button.id).queue()
+                        val roles = ticketButton.ticketButtonConfig.rolesToBeAdded.split(",")
+                        if (roles.isNotEmpty()) {
+                            for (role in roles) {
+
+                                if (event.guild!!.getRolesByName(role, true).isEmpty()) continue
+
+                                val roleObj = event.guild!!.getRolesByName(role, true)[0]
+
+                                it.manager.putPermissionOverride(roleObj,
+                                    mutableListOf(
+                                        Permission.VIEW_CHANNEL,
+                                        Permission.MESSAGE_HISTORY,
+                                        Permission.MESSAGE_ADD_REACTION,
+                                        Permission.MESSAGE_ATTACH_FILES,
+                                        Permission.MESSAGE_SEND
+                                    ),
+                                    mutableListOf()).queue()
+
+                            }
+                        }
+                    }
+                }
+
+            }
+
+            if (ticketButton.ticketButtonConfig.rolesToBePinged != null) {
+
+                val roles = ticketButton.ticketButtonConfig.rolesToBeAdded!!.split(",")
+                if (roles.isNotEmpty()) {
+                    val sb = StringBuilder()
+                    for (role in roles) {
+                        if (event.guild!!.getRolesByName(role, true).isEmpty()) continue
+                        val roleObj = event.guild!!.getRolesByName(role, true)[0]
+                        sb.append(roleObj.asMention).append(",").append(" ")
+                    }
+                    if (sb.isNotBlank())
+                        it.sendMessage(sb.toString()).queue { message ->
+                            message.delete().queueAfter(1, TimeUnit.SECONDS)
+                        }
+                }
+
+            }
 
             if (ticketButton.ticketButtonConfig.openingEmbed != null) {
 
-                it.sendMessageEmbeds(EmbedMessage(ticketButton.ticketButtonConfig.openingEmbed).embedMessage).queue()
+                //TODO: Can apply the member placeholder in anywhere in EmbedMessage
+
+                val builder = EmbedMessageBuilder(ticketButton.ticketButtonConfig.openingEmbed).applyPlaceholderAsTag(event.member!!)
+
+                it.sendMessageEmbeds(builder.build()).queue()
 
             }
 
@@ -38,9 +107,6 @@ class TicketButtonListener(private val bot: Bot): ListenerAdapter() {
                 .setEphemeral(true).queue()
 
         }
-
-        //TODO: Ticket IDs/NOs
-        //TODO: Check for previous tickets.
 
     }
 
