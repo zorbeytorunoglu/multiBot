9956d2cec19a730ca0110fc0d84077f0a76e5a79
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-07-06 04:39:33 +0300

    Added new commands

--- src/main/kotlin/com/zorbeytorunoglu/multiBot/commands/configuration/Commands.kt
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/commands/configuration/Commands.kt
@@ -4,5 +4,13 @@ data class Commands(
     val pingCmd: String = "ping",
     val pingDesc: String = "Shows the ping of the bot.",
     val ticketPanelCmd: String = "ticketpanel",
-    val ticketPanelDesc: String = "Sends a ticket panel to the channel."
+    val ticketPanelDesc: String = "Sends a ticket panel to the channel.",
+    val kickCmd: String = "kick",
+    val kickDesc: String = "Kicks a member.",
+    val banCmd: String = "ban",
+    val banDesc: String = "Bans a member.",
+    val remindCmd: String = "remind",
+    val remindDesc: String = "Schedules a reminder in that channel.",
+    val recordCmd: String = "record",
+    val recordDesc: String = "Records the voices in a voice channel."
 )
