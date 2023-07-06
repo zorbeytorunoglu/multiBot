e1616f7b4497de7cd505b89619ca4e49c0fdf169
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-07-06 04:40:58 +0300

    Added new messages

--- src/main/kotlin/com/zorbeytorunoglu/multiBot/messages/Messages.kt
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/messages/Messages.kt
@@ -7,6 +7,14 @@ data class Messages(
     val ticketPanelIdOption: String = "The ID of the ticket panel (ticketPanels.json)",
     val targetCategoryNotFound: String = "Target category could not be found.",
     val ticketReady: String = "Your ticket is ready! %ticket%",
-    val exceedTicketLimit: String = "You can not exceed your ticket limit!"
+    val exceedTicketLimit: String = "You can not exceed your ticket limit!",
+    val reminder: String = "**Reminder!**",
+    val reminderSet: String = "Reminder set!",
+    val notAudioChannel: String = "That channel is not an audio channel!",
+    val alreadyConnected: String = "Bot is already connected to a channel. Try '/record stop' first.",
+    val recordingStarted: String = "Recording started!",
+    val notRecording: String = "Bot is not recording!",
+    val recordingStopped: String = "Recording is stopped!",
+    val notInVoiceChannel: String = "You are not in a voice channel!"
 
 )
