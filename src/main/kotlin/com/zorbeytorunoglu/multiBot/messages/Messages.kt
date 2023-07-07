5d03558c5e8d8826c33b86c26871745c84f306e6
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-07-07 16:08:53 +0300

    Added somee messages

--- src/main/kotlin/com/zorbeytorunoglu/multiBot/messages/Messages.kt
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/messages/Messages.kt
@@ -15,6 +15,14 @@ data class Messages(
     val recordingStarted: String = "Recording started!",
     val notRecording: String = "Bot is not recording!",
     val recordingStopped: String = "Recording is stopped!",
-    val notInVoiceChannel: String = "You are not in a voice channel!"
+    val notInVoiceChannel: String = "You are not in a voice channel!",
+    val mustBeMemberOrRole: String = "Holder must be a member or a role!",
+    val notValidPermission: String = "It is not a valid permission!",
+    val permissionGiven: String = "Permission %permission% is given to %holder%!",
+    val alreadyHasThisPermission: String = "Subject already has this permission!",
+    val alreadyHasNotThisPermission: String = "Subject does not have this permission already!",
+    val permissionRemoved: String = "Permission %permission% is removed from %holder%!",
+    val hasNoPermission: String = "Subject does not have any permission. If he can use the commands, he may be authorized by the guild.",
+    val permissionCheckTitle: String = "Permission List of %holder%"
 
 )
