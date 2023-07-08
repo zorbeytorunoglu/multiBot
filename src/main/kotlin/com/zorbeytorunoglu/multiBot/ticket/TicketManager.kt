f2bf4e0a6c579cab9cb385d33379549a801d422e
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-07-08 16:08:38 +0300

    Added new methods

--- src/main/kotlin/com/zorbeytorunoglu/multiBot/ticket/TicketManager.kt
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/ticket/TicketManager.kt
@@ -3,7 +3,10 @@ package com.zorbeytorunoglu.multiBot.ticket
 import com.zorbeytorunoglu.multiBot.ticket.configuration.TicketButton
 import com.zorbeytorunoglu.multiBot.ticket.configuration.TicketConfigurationHandler
 import com.zorbeytorunoglu.multiBot.ticket.configuration.TicketPanel
+import net.dv8tion.jda.api.Permission
 import net.dv8tion.jda.api.entities.Member
+import net.dv8tion.jda.api.entities.PermissionOverride
+import net.dv8tion.jda.api.entities.Role
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel
 import net.dv8tion.jda.api.interactions.components.buttons.Button
 
@@ -66,6 +69,16 @@ class TicketManager(val ticketConfigurationHandler: TicketConfigurationHandler) 
 
     }
 
+    fun isTicket(textChannel: TextChannel): Boolean {
+
+        //TODO: Can be improved
+
+        if (textChannel.topic == null) return false
+
+        return textChannel.topic!!.startsWith("ticket-")
+
+    }
+
     fun isMembersTicket(member: Member, textChannel: TextChannel): Boolean {
 
         if (textChannel.topic == null) return false
@@ -97,6 +110,45 @@ class TicketManager(val ticketConfigurationHandler: TicketConfigurationHandler) 
 
     fun exceededTicketLimit(member: Member, buttonId: String): Boolean {
         return getTicketLimit(buttonId) <= getMemberTickets(member).size
+    }
+
+    fun isInTicket(member: Member, channel: TextChannel): Boolean {
+
+        val permissionOverride = getPermissionOverride(member,channel) ?: return false
+
+        return (permissionOverride.allowed.contains(Permission.VIEW_CHANNEL) ||
+                permissionOverride.allowed.contains(Permission.ADMINISTRATOR))
+
+    }
+
+    fun getPermissionOverride(member: Member, textChannel: TextChannel): PermissionOverride? {
+
+        if (textChannel.permissionOverrides.isEmpty()) return null
+
+        textChannel.permissionOverrides.forEach {
+            if (it.member != null) {
+                if (it.member!!.id == member.id) {
+                    return it
+                }
+            }
+        }
+
+        return null
+
+    }
+
+    fun getPermissionOverride(role: Role, textChannel: TextChannel): PermissionOverride? {
+        if (textChannel.permissionOverrides.isEmpty()) return null
+
+        textChannel.permissionOverrides.forEach {
+            if (it.role != null) {
+                if (it.role!!.id == role.id) {
+                    return it
+                }
+            }
+        }
+
+        return null
     }
 
 }
