c031681e8c22711bdeb84fea5deb6856ceaec51a
zorbeytorunoglu <thelegend.mcturk@gmail.com>
2023-07-22 22:15:55 +0300

    Added task management methods

--- src/main/kotlin/com/zorbeytorunoglu/multiBot/task/TaskManager.kt
+++ src/main/kotlin/com/zorbeytorunoglu/multiBot/task/TaskManager.kt
@@ -1,11 +1,15 @@
 package com.zorbeytorunoglu.multiBot.task
 
 import com.zorbeytorunoglu.multiBot.Bot
+import net.dv8tion.jda.api.entities.channel.concrete.Category
 import net.dv8tion.jda.api.entities.channel.concrete.ThreadChannel
+import net.dv8tion.jda.api.entities.channel.unions.GuildChannelUnion
 
 class TaskManager(private val bot: Bot) {
 
-    fun isTicket(threadChannel: ThreadChannel): Boolean {
+    val taskChannels = ArrayList<TaskChannel>()
+
+    fun isTask(threadChannel: ThreadChannel): Boolean {
 
         //TODO: Will be changed
 
@@ -13,9 +17,26 @@ class TaskManager(private val bot: Bot) {
 
     }
 
-    fun getChannel(id: String): ThreadChannel? {
+    fun getTaskChannel(id: String): TaskChannel? {
+        if (taskChannels.isEmpty()) return null
+        return taskChannels.firstOrNull { it.channelId == id }
+    }
 
-        return bot.jda.getGuildChannelById(id)?.guild?.getThreadChannelById(id)
+    fun getTaskChannel(task: Task): TaskChannel? {
+        return taskChannels.find { taskChannel ->
+            taskChannel.tasks.any {
+                it.taskData.taskId == task.taskData.taskId
+            }
+        }
+    }
+
+    fun getTaskChannel(channel: GuildChannelUnion): TaskChannel? {
+        return taskChannels.find { channel.id == it.channelId }
+    }
+
+    fun taskForumExists(category: Category): Boolean {
+
+        return category.forumChannels.any { it.name == bot.settingsHandler.settings.tasksForumChannelName }
 
     }
 
